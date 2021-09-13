c41fe818a147b17dd649c4e5ef769fb3bba59eff
iwu <iriswu0819@gmail.com>
2021-09-13 14:45:01 -0400

    footer part and to-top btn

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ document.querySelector(".hamburger-menu")
   document.querySelector(".container").classList
   .toggle('change')
 })
+
+document.querySelector(".scroll-btn").addEventListener
+("click", ()=> {
+  document.querySelector("html").style
+  .scrollBehavior = "smooth";
+  setTimeout(()=>{
+    document.querySelector("html").style
+    .scrollBehavior = "unset";
+  }, 1000)
+})
